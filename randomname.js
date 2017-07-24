e7d3f63dee2bae817d635f0236828fd5aa6c3b76
Bob the Hamster <Bob@HamsterRepublic.com>
2017-07-24 09:36:44 -0700

    Add synthesizeName() to splice together two random names with a common two-character segment

--- randomname.js
+++ randomname.js
@@ -21,22 +21,41 @@ app.controller('myCtrl', function($scope, $http) {
         $scope.namesList = response.data.split("\n");
         
         $scope.randomName = function() {
-            var name = randomFromArray($scope.namesList);
+            let name = randomFromArray($scope.namesList);
             $scope.randomCount += 1;
             return name;
         }
 
         $scope.regenerate();
 
-
     });
+    
+    $scope.synthesizeName = function () {
+        let name1 = $scope.randomName();
+        for(let i = 0; i < 10; i++) {
+            let segmentOffset = randomRange(1, name1.length - 2);
+            let segment = name1.substring(segmentOffset, segmentOffset + 2);
+            // Try 100 times to find another name that can be grafted together on the segment
+            for(let i = 0; i < 100; i++) {
+                let name2 = $scope.randomName().toLowerCase();
+                let foundAt = name2.indexOf(segment);
+                if (foundAt >= 0) {
+                    outName = name1.substring(0, segmentOffset) + name2.substring(foundAt, name2.length)
+                    return outName;
+                }
+            }
+        }
+        // After 1000 attempts, it is time to give up, and just return the first name unmodified
+        return name1;
+    }
 
     $scope.regenerate = function () {
-        var outNames = [];
-        for (i = 0; i < 10; i++) {
-            var row = [];
-            for (j = 0; j < 10 ; j++) {
-                row.push($scope.randomName());
+        outNames = [];
+        for (let i = 0; i < 10; i++) {
+            let row = [];
+            for (let j = 0; j < 10 ; j++) {
+                //row.push($scope.randomName());
+                row.push($scope.synthesizeName());
             }
             outNames.push(row);
         }
